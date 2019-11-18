645082d17b871dc79cc225df7ee34565588b2de6
Vishal Kushwaha <vishsinkush@gmail.com>
2019-11-19 01:43:02 +0530

    delete faust code

--- streamio/stream-consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer(bootstrap_servers='172.17.48.181:9092',
-                         auto_offset_reset='earliest',
-                         consumer_timeout_ms=1000)
-consumer.subscribe(['input_final'])
-for msg in consumer:
-    print(msg.value)
-consumer.close()
--- streamio/stream-generator.py
+++
@@ -1,8 +0,0 @@
-from kafka import KafkaProducer
-from time import sleep
-
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-for _ in range(10):
-	producer.send('input_first', b'this is a message') 
-
-producer.close()
